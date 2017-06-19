ea7eddfe6dbc84643d04d2246ad4cd4f8f23b5b8
Stefano Zaghi <stefano.zaghi@gmail.com>
2017-06-19 18:01:48 +0200

    sanitize and add a lot of doctests

--- src/tests/kinds.f90
+++ src/tests/kinds.f90
@@ -12,8 +12,6 @@ type(vector) :: vector1 !< Vector dummy variable.
 type(vector) :: vector2 !< Vector dummy variable.
 type(vector) :: vector3 !< Vector dummy variable.
 
-call vector1%set(x=-1._R_P, y=-1._R_P, z=-1._R_P)
-call vector1%init()
 print "(A)", ' Assign vector1 = [1, 2, 3]'
 vector1 = 1 * ex + 2 * ey + 3 * ez
 print "(A)", ' Assign vector2 = [1, 1, 1] testing assignments by scalar'
@@ -39,10 +37,10 @@ vector1 = vector1 * 1_I8P
 vector1 = vector1 * 1_I4P
 vector1 = vector1 * 1_I2P
 vector1 = vector1 * 1_I1P
-call vector1%print
+call vector1%printf
 print "(A)", ' Verify * operator between vectors, vector1 * vector2'
 vector1 = vector1 * vector2
-call vector1%print
+call vector1%printf
 print "(A)", ' Verify / operator, diveded by 1 defined in any supported number formats'
 vector1 = vector1 / 1._R16P
 vector1 = vector1 / 1._R8P
@@ -51,10 +49,10 @@ vector1 = vector1 / 1_I8P
 vector1 = vector1 / 1_I4P
 vector1 = vector1 / 1_I2P
 vector1 = vector1 / 1_I1P
-call vector1%print
+call vector1%printf
 print "(A)", ' Verify / operator between vectors, vector1 / vector2'
 vector1 = vector1 / vector2
-call vector1%print
+call vector1%printf
 print "(A)", ' Verify + operator, add 1 (14 times) defined in any supported number formats'
 vector1 = 1._R16P + vector1
 vector1 = 1._R8P + vector1
@@ -71,10 +69,10 @@ vector1 = vector1 + 1_I8P
 vector1 = vector1 + 1_I4P
 vector1 = vector1 + 1_I2P
 vector1 = vector1 + 1_I1P
-call vector1%print
+call vector1%printf
 print "(A)", ' Verify + operator between vectors, vector1 + vector2'
 vector1 = vector1 + vector2
-call vector1%print
+call vector1%printf
 print "(A)", ' Verify - operator, subtract 1 (14 times) defined in any supported number formats'
 vector1 = 1._R16P - vector1
 vector1 =-1._R8P - vector1
@@ -91,25 +89,25 @@ vector1 = vector1 - 1_I8P
 vector1 = vector1 - 1_I4P
 vector1 = vector1 - 1_I2P
 vector1 = vector1 - 1_I1P
-call vector1%print
+call vector1%printf
 print "(A)", ' Verify - operator between vectors, vector1 - vector2'
 vector1 = vector1 - vector2
-call vector1%print
+call vector1%printf
 print "(A)", ' Verify save/load methods'
 open(unit=2, form='UNFORMATTED', status='SCRATCH')
-call vector1%save(unit=2)
+call vector1%save_into_file(unit=2)
 rewind(unit=2)
-call vector3%load(unit=2)
+call vector3%load_from_file(unit=2)
 close(unit=2)
-call vector3%print
+call vector3%printf
 vector3 = 0
 print "(A)", ' Verify save/load methods with stream-accessed file'
 open(unit=2, form='UNFORMATTED', status='SCRATCH', access='STREAM')
-call vector1%save(unit=2, pos=1_I8P)
+call vector1%save_into_file(unit=2, pos=1_I8P)
 rewind(unit=2)
-call vector3%load(unit=2, pos=1_I8P)
+call vector3%load_from_file(unit=2, pos=1_I8P)
 close(unit=2)
-call vector3%print
+call vector3%printf
 print "(A)", ' Verify < operator, compare with 1 (and vector2) defined in any supported number formats'
 print "(A,L1)", 'vector1<1._R16P => ', (vector1<1._R16P)
 print "(A,L1)", 'vector1<1._R8P  => ', (vector1<1._R8P )
--- src/tests/simple.f90
+++ src/tests/simple.f90
@@ -20,20 +20,20 @@ print "(A,F5.1)", ' vector1%normL2() = ', vector1%normL2()
 print "(A,F5.1)", ' normL2(vector1) = ', normL2(vector1)
 print "(A)", ' normalized(vector1):'
 vector3 = vector1%normalized()
-call vector3%print
+call vector3%printf
 print "(A)", 'call vector1%normalize():'
 vector3 = vector1
 call vector3%normalize()
-call vector3%print
+call vector3%printf
 print "(A)", ' Verify normalization fall-back for null vector'
 vector3 = 0
 call vector3%normalize()
 print "(A)", 'call 0%normalize():'
-call vector3%print
+call vector3%printf
 vector3 = 0
 vector3 = vector3%normalized()
 print "(A)", 'vector1 = 0%normalized():'
-call vector3%print
+call vector3%printf
 print "(A)", ' Verify dot product'
 print "(A,F5.1)", ' vector1.dot.ex = ', vector1.dot.ex
 print "(A,F5.1)", ' vector1.dot.ey = ', vector1.dot.ey
@@ -42,27 +42,27 @@ print "(A,F5.1)", ' vector1.dot.vector2 = ', vector1.dot.vector2
 print "(A)", ' Verify cross product'
 print "(A)", ' vector1.cross.vector2:'
 vector3 = vector1.cross.vector2
-call vector3%print
+call vector3%printf
 print "(A)", ' Verify special operators'
 print "(A)", ' vector1.ortho.vector2:'
 vector3 = vector1.ortho.vector2
-call vector3%print
+call vector3%printf
 print "(A)", ' vector1.paral.vector2:'
 vector3 = vector1.paral.vector2
-call vector3%print
+call vector3%printf
 print "(A)", ' Verify * / + - operators'
 print "(A)", ' vector1 * vector2:'
 vector3 = vector1 * vector2
-call vector3%print
+call vector3%printf
 print "(A)", ' vector1 / vector2:'
 vector3 = vector1 / vector2
-call vector3%print
+call vector3%printf
 print "(A)", ' vector1 + vector2:'
 vector3 = vector1 + vector2
-call vector3%print
+call vector3%printf
 print "(A)", ' vector1 - vector2:'
 vector3 = vector1 - vector2
-call vector3%print
+call vector3%printf
 print "(A)", ' Assign vector1 = [-1, 1, 0]'
 vector1 = -ex + ey
 print "(A)", ' Assign vector2 = [0, 1, 0]'
@@ -73,28 +73,28 @@ print "(A)", ' Assign vector4 = [-1, -1, 0]'
 vector4 = -ex - ey
 print "(A)", ' Face 1-2-3 normal (stand alone procedure):'
 vector5 = face_normal3(pt1=vector1, pt2=vector2, pt3=vector3)
-call vector5%print
+call vector5%printf
 print "(A)", ' Face 1-2-3 normal (stand alone procedure, normalized):'
 vector5 = face_normal3(norm='y', pt1=vector1, pt2=vector2, pt3=vector3)
-call vector5%print
+call vector5%printf
 print "(A)", ' Face 1-2-3 normal (type bound method):'
 call vector5%face_normal3(pt1=vector1, pt2=vector2, pt3=vector3)
-call vector5%print
+call vector5%printf
 print "(A)", ' Face 1-2-3 normal (type bound method, normalized):'
 call vector5%face_normal3(norm='y', pt1=vector1, pt2=vector2, pt3=vector3)
-call vector5%print
+call vector5%printf
 print "(A)", ' Face 1-2-3-4 normal (stand alone procedure): '
 vector5 = face_normal4(pt1=vector1, pt2=vector2, pt3=vector3, pt4=vector4)
-call vector5%print
+call vector5%printf
 print "(A)", ' Face 1-2-3-4 normal (stand alone procedure, normalized): '
 vector5 = face_normal4(norm='y', pt1=vector1, pt2=vector2, pt3=vector3, pt4=vector4)
-call vector5%print
+call vector5%printf
 print "(A)", ' Face 1-2-3-4 normal (type bound method):'
 call vector5%face_normal4(pt1=vector1, pt2=vector2, pt3=vector3, pt4=vector4)
-call vector5%print
+call vector5%printf
 print "(A)", ' Face 1-2-3-4 normal (type bound method, normalized):'
 call vector5%face_normal4(norm='y', pt1=vector1, pt2=vector2, pt3=vector3, pt4=vector4)
-call vector5%print
+call vector5%printf
 print "(A,I3)", ' IO length of vector1 = ', vector1%iolen()
 
 stop
